67111f928f85e6bd71ecf427bdf8e693f10b9eaa
Prakhar <psahu9215@sdsu.edu>
2026-03-06 11:28:08 -0800

    Added Swift Files
    Baseline for the integration of the "bridge" of the iPhone camera using Swift with WPF

---
+++ Table Tennis Trainer/Table Tennis Trainer/Table_Tennis_TrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Table_Tennis_TrainerApp.swift
+//  Table Tennis Trainer
+//
+//  Created by Prakhar Sahu on 3/6/26.
+//
+
+import SwiftUI
+
+@main
+struct Table_Tennis_TrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
