da899b647516cb766a9d36da7017fa5e9cdd0aeb
Prakhar <psahu9215@sdsu.edu>
2026-03-16 14:19:25 -0700

    Implement PoseSender for network data transmission
    Added PoseSender class for network communication and joint data sending.

--- Table Tennis Trainer/Table Tennis Trainer/Table_Tennis_TrainerApp.swift
+++ Table Tennis Trainer/Table Tennis Trainer/Table_Tennis_TrainerApp.swift
@@ -1,17 +1,28 @@
-//
-//  Table_Tennis_TrainerApp.swift
-//  Table Tennis Trainer
-//
-//  Created by Prakhar Sahu on 3/6/26.
-//
+//  Created by Prakhar Sahu
 
 import SwiftUI
 
 @main
-struct Table_Tennis_TrainerApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+import Network
+import ARKit
+
+class PoseSender {
+    var connection: NWConnection?
+    let pcIP: String = "" // Dion needs to provide his IP
+    let port: NWEndpoint.Port = 8888
+
+    init() {
+        let host = NWEndpoint.Host(pcIP)
+        connection = NWConnection(host: host, port: port, using: .udp)
+        connection?.start(queue: .global())
+    }
+
+    func sendJointData(jointName: String, x: Float, y: Float) {
+        let message = "\(forehand):\(x),\(y)"
+        let data = message.data(using: .utf8)
+        
+        connection?.send(content: data, completion: .contentProcessed({ error in
+            if let error = error { print("Send error: \(error)") }
+        }))
     }
 }
